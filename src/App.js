bd51881682802613dce895da573113efbc936503
Maciej_Niemiec <niemiec.maciej@gmail.com>
2023-01-19 22:10:01 +0100

    handle error and loading message

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://reqres.in/api/products")
-      .then((products) => products.json())
+      .then((products) => {
+        const fetchError = !products.ok;
+        if (fetchError) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        return products.json();
+      })
       .then((data) => {
         setProducts(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div>
+      {error && <p>{error}</p>}
+      {isLoading && <p>Loading...</p>}
       {products &&
         products.data.map((product, index) => {
           return <p key={index}>{product.name}</p>;
