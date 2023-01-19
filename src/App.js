5fecdb0d6ada9747dc7798d359c506f257d995f4
Maciej_Niemiec <niemiec.maciej@gmail.com>
2023-01-19 20:14:50 +0100

    render a product list using fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/products")
+      .then((products) => products.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products &&
+        products.data.map((product, index) => {
+          return <p key={index}>{product.name}</p>;
+        })}
     </div>
   );
 }
